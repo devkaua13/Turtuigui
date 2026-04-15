ae27e261f7ecf75c5eb016bfcd939c57099127ac
KAUAVINICIOS0 <kaua.vinicios_2005@hotmail.com>
2026-04-15 15:10:54 -0300

    feat: resolve exercise 2 from course Python Impressionador

---
+++ exercise/comparacao_com_ano_anterior.py
@@ -0,0 +1,22 @@
+vendas_produtos = [('iphone', 558147, 951642),
+                   ('galaxy', 712350, 244295), 
+                   ('ipad', 573823, 26964), 
+                   ('tv', 405252, 787604), 
+                   ('máquina de café', 718654, 867660), 
+                   ('kindle', 531580, 78830), 
+                   ('geladeira', 973139, 710331), 
+                   ('adega', 892292, 646016), 
+                   ('notebook dell', 422760, 694913), 
+                   ('notebook hp', 154753, 539704), 
+                   ('notebook asus', 887061, 324831), 
+                   ('microsoft surface', 438508, 667179), 
+                   ('webcam', 237467, 295633), 
+                   ('caixa de som', 489705, 725316), 
+                   ('microfone', 328311, 644622), 
+                   ('câmera canon', 591120, 994303)]
+
+for nome, ano2019, ano2020 in vendas_produtos:
+    if ano2020>ano2019:
+        crescimento = ano2020/ano2019 - 1
+        print("{} teve {} vendas em 2019, {} vendas em 2020. Crescimento de {:.1%} \n" .format(nome, ano2019, ano2020, crescimento))
+
